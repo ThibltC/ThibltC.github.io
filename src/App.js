8ed987ece09607635ccd275437096fab21d5677b
ThibltC <yojibo@hotmail.fr>
2020-10-19 22:00:32 +0200

    [add] work is progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import WorkInProgress from './static/images/work-in-progress-wip.jpg';
 import informations from './informations';
 import MyInformations from './components/MyInformations';
 
 
 const App = () => {
   return (
-    <div>
-      <MyInformations />
+    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {/* <MyInformations /> */}
+      <img src={WorkInProgress} alt='work-in-progress'/>
     </div>
   )
 }
